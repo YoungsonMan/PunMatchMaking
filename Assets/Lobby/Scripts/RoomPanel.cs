00dfc52efc57782a945de8e0d19295996fca47c8
YoungsonMan <choi13570@naver.com>
2024-11-13 18:38:13 +0900

    Add : Lobby 기본 Pacakge 추가
    기본 연결만된 패키지 추가

---
+++ Assets/Lobby/Scripts/RoomPanel.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomPanel : MonoBehaviour
+{
+    [SerializeField] PlayerEntry[] playerEntries;
+    [SerializeField] Button startButton;
+
+    public void StartGame()
+    {
+
+    }
+
+    public void LeaveRoom()
+    {
+
+    }
+}
